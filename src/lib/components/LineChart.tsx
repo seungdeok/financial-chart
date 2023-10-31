ea429f040533bd66a22963e75268baab28ab5a13
seungdeok <akk9132@naver.com>
2023-10-31 22:51:55 +0900

    feat: Add LineChart

---
+++ src/lib/components/LineChart.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Chart from "react-google-charts";
+
+export interface Props {
+  data: string[] | number[][];
+  width: string | number;
+  height: string | number;
+  options: any;
+}
+
+export const LineChart = ({
+  width = "100%",
+  height = "100%",
+  data,
+  options,
+}: Props) => {
+  return (
+    <Chart
+      chartType="LineChart"
+      data={data}
+      width={width}
+      height={height}
+      options={options}
+    />
+  );
+};
