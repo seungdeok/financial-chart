ee625e4a72492eeccc4a14167b5e2d001c8173d4
seungdeok <akk9132@naver.com>
2023-11-08 23:15:28 +0900

    feat: Add AreaChart story

--- src/lib/components/AreaChart.tsx
+++ src/lib/components/AreaChart.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Chart from "react-google-charts";
 
-export interface Props {
-  data: string[] | number[][];
-  width: string | number;
-  height: string | number;
-  options: any;
+export interface AreaChartProps {
+  data: (string | number)[][];
+  width?: string | number;
+  height?: string | number;
+  options?: any;
 }
 
 export const AreaChart = ({
   width = "100%",
   height = "100%",
   data,
-  options,
-}: Props) => {
+  options = {},
+}: AreaChartProps) => {
   return (
     <Chart
       chartType="AreaChart"
