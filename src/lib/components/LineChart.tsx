b5168f27018445333ea1542a65e1ee73fab92717
seungdeok <akk9132@naver.com>
2023-11-09 20:41:47 +0900

    feat: Add LineChart story

--- src/lib/components/LineChart.tsx
+++ src/lib/components/LineChart.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Chart from "react-google-charts";
 
-export interface Props {
-  data: string[] | number[][];
-  width: string | number;
-  height: string | number;
-  options: any;
+export interface LineChartProps {
+  data: (string | number)[][];
+  width?: string | number;
+  height?: string | number;
+  options?: any;
 }
 
 export const LineChart = ({
   width = "100%",
   height = "100%",
   data,
-  options,
-}: Props) => {
+  options = {},
+}: LineChartProps) => {
   return (
     <Chart
       chartType="LineChart"
